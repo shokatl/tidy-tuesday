dddd5f24658716083b9c5659b9234ed2e9e5d983
Leila Shokat <shokatl@gmail.com>
2020-12-04 19:41:09 -0800

    made a leaflet map to show hikes data

--- 2020/2020-11-24.R
+++ 2020/2020-11-24.R
@@ -5,6 +5,9 @@ library(zoo)
 library(ggtext)
 library(patchwork)
 library(stringr)
+library(ggmap)
+library(maps)
+library(leaflet)
 
 pip <- tt_load("2020-11-24")
 
@@ -20,4 +23,46 @@ hikes1 <- hikes %>%
          highpoint = as.numeric(highpoint))%>%
   filter(rating > 4)
 
+place <- hikes1 %>%
+  select(name) %>%
+  mutate_geocode(name)
 
+hike_locations <- left_join(place, hikes1) %>%
+  drop_na(lat, lon)
+
+hike_map_box <- c(bottom = min(hike_locations$lat), left = min(hike_locations$lon), 
+                  top = max(hike_locations$lat), right = max(hike_locations$lon))
+
+hike_map <- get_stamenmap(hike_map_box, 
+                          maptype = "terrain-background",
+                          zoom = 5)
+hike_map %>% 
+  ggmap()
+
+
+hike_map %>% 
+  ggmap() +
+  geom_point(data = hike_locations, aes(lon, lat,
+                                        size = length,
+                                        color = rating),
+             inherit.aes = FALSE, alpha = 0.5) +
+  theme_void()
+
+hiker  <- makeIcon(
+  iconUrl = "https://openclipart.org/download/11401/johnny-automatic-NPS-map-pictographs-part-95.svg",
+  iconWidth = 20, iconHeight = 40)
+hiker
+
+content <- paste("<b>", hike_locations$name, 
+                 "</b></br>", "Length:",
+                 hike_locations$length,
+                 "</br>", "Gain:",
+                 hike_locations$gain,
+                 "</br>", "Rating:",
+                 hike_locations$rating)
+
+leaflet() %>%
+  addTiles() %>%
+  addMarkers(lng = ~lon, lat = ~lat, 
+             data = hike_locations, 
+             popup = content, icon = hiker)
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### December 1, 2020 - Toronto homeless shelters
 
 ![ ](2020/2020-12-01.png)
+
+![](file://localhost/Users/leilashokat/Desktop/mLab/2020-2021/TidyTuesday/tidy-tuesday/2020/2020-11-24.html)
