c90a9f28ca8258c46541ceddf71a23bef0211d3a
Leila Shokat <shokatl@gmail.com>
2020-12-07 22:30:09 -0800

    Started on ikea data

---
+++ 2020/2020-11-03.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(tidytuesdayR)
+library(lubridate)
+library(zoo)
+library(ggtext)
+library(patchwork)
+library(viridis)
+
+pip <- tt_load("2020-11-03")
+
+ikea <- pip$ikea
+
+
