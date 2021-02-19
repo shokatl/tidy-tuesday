8311ee72928474c167ee0b9b168b04d958efd39a
Leila Shokat <shokatl@gmail.com>
2021-02-18 17:04:44 -0800

    Almost done with first plot (figure out how to change font/bring extra fonts into R for ggplot2)

--- 2021/2021-02-16.R
+++ 2021/2021-02-16.R
@@ -15,10 +15,24 @@ georgia_pop <- pip$georgia_pop %>%
 ggplot(data = georgia_pop, mapping = aes(x = Year, y = Pop, lty = Race)) +
   geom_line() +
   coord_flip() +
+  scale_y_reverse(n.breaks = 19, expand=c(0,0), minor_breaks = FALSE) +
+  scale_x_continuous(n.breaks = 9, limits = c(1790, 1890), expand=c(0,0), minor_breaks = FALSE) +
+  #scale_y_continuous(n.breaks = 19, expand=c(0,0), minor_breaks = FALSE) +
   theme(
-    plot.background = element_rect(color = "#FAD8BA", fill = "#FAD8BA"),
-    panel.background = element_rect(color = "#FAD8BA", fill = "#FAD8BA"),
-    legend.background = element_rect(color = "#FAD8BA", fill = "#FAD8BA"),
+    #title = element_text(family = "URWGothic"),
+    plot.background = element_rect(color = "#F0D6B7", fill = "#F0D6B7"),
+    panel.background = element_rect(color = "#F0D6B7", fill = "#F0D6B7"),
+    legend.background = element_rect(color = "#F0D6B7", fill = "#F0D6B7"),
+    legend.key = element_rect(color = "#F0D6B7", fill = "#F0D6B7"),
     panel.border = element_rect(color = "black", fill = "transparent"),
-    panel.grid.major = element_line(color = "red")
+    panel.grid.major = element_line(color = "red", size = .2),
+    legend.position = "bottom"
+  ) +
+  labs(
+    x = "",
+    y = "PERCENTS",
+    lty = "",
+    title = "COMPARATIVE INCREASE OF WHITE AND COLORED \nPOPULATION OF GEORGIA."
   )
+  
+
