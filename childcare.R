25f930534118cdb6fb8cfd881c073927b8ad4555
Leila Shokat <60753772+shokatl@users.noreply.github.com>
2023-05-13 20:53:31 -0700

    exploring childcare data

--- childcare.R
+++ childcare.R
@@ -21,7 +21,7 @@ costs_over_time <- costs %>%
          ) %>%
   select(!study_year) %>%
   left_join(counties) %>%
-  filter(county_name == "Multnomah County") %>%
+  filter(county_name == "Deschutes County") %>%
   mutate(weekly_income = mhi_2018/52)
   
   
@@ -31,8 +31,41 @@ ggplot(costs_over_time, mapping = aes(x = year, y = mcsa)) +
 ggplot(costs_over_time, mapping = aes(x = year, y = mc_infant)) +
   geom_col()
 
-ggplot(costs_over_time, mapping = aes(x = year, y = weekly_income)) +
-  geom_col() +
-  geom_col(inherit.aes = FALSE, mapping = aes(x = year, y = mc_infant, fill = "red"))
+#calculating education costs as a proportion of weekly income in all counties:
+
+props_all <- costs %>%
+  select(county_fips_code, study_year, mcsa,
+         fme_2018, h_under6_single_m, households,
+         mc_infant, mc_toddler,
+         pr_f, pr_p, mhi_2018) %>%
+  mutate(county_fips_code = as.factor(county_fips_code),
+         mcsa = as.numeric(mcsa),
+         mc_infant = as.numeric(mc_infant),
+         mc_toddler = as.numeric(mc_toddler)) %>%
+  left_join(counties) %>%
+  mutate(weekly_income = mhi_2018/52,
+         wi_fe = fme_2018/52) %>%
+  mutate(prop_sa = mcsa/weekly_income,
+         prop_infant = mc_infant/weekly_income,
+         prop_toddler = mc_toddler/weekly_income,
+         percent_single_m = (h_under6_single_m/households)*100)
+  
+#looking at only most recent year
+props_2018 <- props_all %>%
+  filter(study_year == "2018")
+
+ggplot(props_2018, mapping = aes(x = weekly_income, y = mcsa)) +
+  geom_point(color = "red", alpha = 0.3) +
+  geom_point(mapping = aes(x = weekly_income, y = mc_infant), color = "blue", alpha = 0.3) +
+  geom_point(mapping = aes(x = weekly_income, y = mc_toddler), color = "green", alpha = 0.3)
+
+ggplot(props_2018, mapping = aes(x = pr_f, y = mc_infant)) +
+  geom_point(color = "maroon", alpha = 0.5)
+
+ggplot(props_2018, mapping = aes(x = weekly_income, y = mc_infant)) +
+  geom_point(color = "maroon", alpha = 0.5)
+
+ggplot(props_2018, mapping = aes(x = percent_single_m, y = fme_2018)) +
+  geom_point(color = "maroon", alpha = 0.5)
 
 
